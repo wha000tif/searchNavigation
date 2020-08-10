31d0af92844a4781c891d420ee2e0fce24f2631c
Conanjun <1526840124@qq.com>
2020-08-10 17:26:55 +0800

    finish openlinks

--- static/js/index.js
+++ static/js/index.js
@@ -4,28 +4,30 @@ $(function () {
 
 });
 
-function getUrlParam(name){
+function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]); return null;
 }
 
-function reset(){
+function reset() {
     window.location.href = "/";
 }
 
-function goSearch(){
+function goSearch() {
     var kw = document.getElementById("searchKey").value;
-    if(kw && kw!= null && kw!="null"){
+    if (kw && kw != null && kw != "null") {
         // window.location.href = encodeURI(encodeURI("list.html?kw=" + kw));
         alert("success");
-    } else{
+    } else {
         // window.location.href = "index.html";
         alert("failed");
     }
 }
 
-function openAlllinks(links_div_name){
-    //根据传入的div name遍历其中的a标签并打开
+function openAlllinks(links_div_name) {
+    $("#" + links_div_name + " ul li a").each(function () {
+        window.open($(this).attr('href'), '_blank').location
+    });
 
 }
