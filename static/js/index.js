f08f9dad0a9313e4ead88f4f5574f2369e412b62
wha000tif <994873406@qq.com>
2020-08-10 18:35:49 +0800

    修改index.js 修改Hunter_infoGather_Helper.html

--- static/js/index.js
+++ static/js/index.js
@@ -15,10 +15,12 @@ function reset(){
 }
 
 function goSearch(){
-    var kw = document.getElementById("searchKey").value;
+    var kw = document.getElementById("searchKeyGoogle").value;
     if(kw && kw!= null && kw!="null"){
         // window.location.href = encodeURI(encodeURI("list.html?kw=" + kw));
-        alert("success");
+        googleSite(kw);
+        googleSubdomain(kw);
+        githubSearch(kw);
     } else{
         // window.location.href = "index.html";
         alert("failed");
@@ -28,4 +30,21 @@ function goSearch(){
 function openAlllinks(links_div_name){
     //根据传入的div name遍历其中的a标签并打开
 
+}
+
+
+function googleSite(keyword){
+    var query = "https://www.google.com.hk/search?q=site%3A"+keyword+"&rlz=1C1CHWL_zh-CNIQ879IQ879" 
+    document.getElementById("googleSite").href=query;
+}
+
+
+function googleSubdomain(keyword){
+    var query = "https://www.google.com.hk/search?q=site%3A*."+keyword+"&rlz=1C1CHWL_zh-CNIQ879IQ879" 
+    document.getElementById("googleSubdomain").href=query;
+}
+
+function githubSearch(keyword){
+    var query = "https://github.com/search?q="+keyword+"&type=Code"
+    document.getElementById("githubSearch").href=query;
 }
